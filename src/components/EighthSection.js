d0eb7ee43d0a4ae2a16007fbf8d56774e58e3603
igortargino <igorfurtadot@gmail.com>
2021-12-14 22:05:57 -0300

    Ações do formulário

--- src/components/EighthSection.js
+++ src/components/EighthSection.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SixthSection() {
+
+    const [name, setName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+
+    function saveName(event) {
+        let inputName = event.target.value;
+        setName(inputName);
+    };
+
+    function saveCpf(event) {
+        let inputCpf = event.target.value;
+        setCpf(inputCpf);
+    };
+
+    function saveEmail(event) {
+        let inputEmail = event.target.value;
+        setEmail(inputEmail);
+    };
+
+    function savePhone(event) {
+        let inputPhone = event.target.value;
+        setPhone(inputPhone);
+    };
+
+    function cleanInputs() {
+        setTimeout(() => {
+            setName("");
+            setCpf("");
+            setEmail("");
+            setPhone("");
+        }, 2000);
+    };
+
     return (
         <section className="section-module content" style={{ position: "relative" }}>
             <div className="orange-box-2" style={{ position: "absolute" }}></div>
@@ -11,13 +46,13 @@ export default function SixthSection() {
                             <h2 className="sora-form-title" style={{ marginBottom: "10px" }}>Quero fazer parte</h2>
                             <h4 className="sora-form-descriptions">Preencha os campos abaixo com<br />os seus dados para iniciar o processo<br /> de abertura de conta.</h4>
                         </div>
-                        <div className="form-inputs" id="form-contact">
-                            <input type="text" placeholder="Nome Completo*" required></input>
-                            <input type="number" placeholder="CPF*" required></input>
-                            <input type="email" placeholder="E-mail*" required></input>
-                            <input type="tel" placeholder="Telefone"></input>
-                            <button className="btn-form">Enviar</button>
-                        </div>
+                        <form className="form-inputs" id="form-contact" action="https://hotmail.us20.list-manage.com/subscribe/post?u=704f189af161a694eaba5fa5c&amp;id=00344dfba9" method="post" name="mc-embedded-subscribe-form" target="_blank">
+                            <input value={name} onChange={saveName} type="text" placeholder="Nome Completo*" required name="NAME"></input>
+                            <input value={cpf} onChange={saveCpf} type="text" placeholder="CPF (apenas números)*" required name="CPF"></input>
+                            <input value={email} onChange={saveEmail} type="email" placeholder="E-mail*" required name="EMAIL"></input>
+                            <input value={phone} onChange={savePhone} type="tel" placeholder="Telefone" name="PHONE"></input>
+                            <button className="btn-form" onClick={cleanInputs}>Enviar</button>
+                        </form>
                         <div>
                             <h4 className="sora-form-descriptions">Ao continuar, você permite que o banco<br />digital Deo X entre em contato com você<br />pelos canais informados.</h4>
                         </div>
@@ -37,6 +72,7 @@ export default function SixthSection() {
                     </div>
                 </div>
             </div>
-        </section >
+        </section>
     )
 };
+
