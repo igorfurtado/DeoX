6021d37709aa784616adecc8dc4fa22c45c0c47c
igortargino <igorfurtadot@gmail.com>
2022-01-12 18:33:11 -0300

    Edição do footer e correções da responsividade

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,20 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            <div className="footer-box">
-                <div style={{ width: "80%" }}>
-                    <hr style={{ border: "1px solid gray" }}></hr>
-                    <p className="paragraph-poppins" style={{ color: "#EDE8E8", fontWeight: 100 }}>© Deo X Banco Digital </p>
-                </div>
+        <footer className="footer-box">
+            <div className="footer-content">
+                <span className="footer-title">Fale com a gente</span>
+
+                <span className="footer-section1 footer-margin">SAC</span>
+
+                <span className="footer-section1">0800 xxx xxxx</span>
+                <span className="footer-section1 footer-margin">sac@deoxbank.com</span>
+                <hr className="footer-line"></hr>
+                <span className="footer-section2">© 2022 Deo X - Banco Digital. CNPJ: 41.471.766/0001-31 </span>
+
+                <span className="footer-section2 footer-address footer-margin">Rua Ana Guedes Vasconcelos, 81, sala 703C, Altiplano Cabo Branco,
+                    58046-092 - João Pessoa, PB.</span>
             </div>
-        </div>
+        </footer>
     );
 };
